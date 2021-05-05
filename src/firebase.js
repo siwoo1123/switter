dc474fa25830d03c3579318d79b557f6ff51f0d2
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2021-05-05 09:29:26 +0900

    abs

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAAVxEItaUOKlcggCqv-vG5m3h_XgWilCU",
+    authDomain: "nwitter-d02e0.firebaseapp.com",
+    projectId: "nwitter-d02e0",
+    storageBucket: "nwitter-d02e0.appspot.com",
+    messagingSenderId: "652207168073",
+    appId: "1:652207168073:web:9b908766dbbcb92a963d98"
+};
+
+firebase.initializeApp(firebaseConfig);
